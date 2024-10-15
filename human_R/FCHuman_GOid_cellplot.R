7c282cb4b34767b1969fd03cc2548990d03b9580
babak-jamshidikia <jamshidikia@gmail.com>
2024-10-15 14:58:47 -0700

    update cell plot

--- human_R/FCHuman_GOid_cellplot.R
+++ human_R/FCHuman_GOid_cellplot.R
@@ -116,7 +116,9 @@ topDiffGenes <- function(allScore) {
 }
 
 #funcGO <-function(ont,TopN,P_valcutoff,updown){
-pdf(file = "/home/bjamshidikia/Desktop/BIOINFORMATICS_jacobi/graphs/cellplot/allcellplot-6.pdf",height = 5.96, width = 10)
+#paper="a4"
+#pdf(file = "/home/bjamshidikia/Desktop/BIOINFORMATICS_jacobi/graphs/cellplot/allcellplotplot-15.pdf",height = 5.96, width = 10)
+pdf(file = "/home/bjamshidikia/Desktop/BIOINFORMATICS_jacobi/graphs/cellplot/allcellplotplot-15.pdf",paper="letter")
 for(onto in c("BP","CC","MF")){
   
 
@@ -161,106 +163,121 @@ updown = "both"
   
   allresUpbasicplot <- allResUp
   allresUpbasicplot$Elim <- -log2(as.numeric(allResup2$Elim))
+  #allresUpbasicplot <- subset(allresUpbasicplot,Elim <= 0.05 & Significant > 20)
   
-  basic_plot <- ggplot(allresUpbasicplot, aes(x = reorder(Term,Elim),y= Elim)) +
-    geom_bar(stat = "identity", fill = graphfill) + coord_flip() +
-    labs(title = paste0("SIC vs SIC_TM \n",ont," ",updown),
-         x = "Term",
-         y = "-log2(pval)")
+  #basic_plot <- ggplot(allresUpbasicplot, aes(x = reorder(Term,Elim),y= Elim)) +
+  #  geom_bar(stat = "identity", fill = graphfill) + coord_flip() +
+  #  labs(title = paste0("            SIC vs SIC_TM \n",ont," ",updown," up and down regulated genes"),
+  #       x = "Term",
+  #       y = "-log2(pval)")
   
   
-  print(basic_plot)
+  #print(basic_plot)
   
   
   
   
   
   #allResup2$Elim <- -log2(as.numeric(allResup2$Elim))
-  allResup2$LogEnrich <-  log2(allResup2$Significant/allResup2$Expected)
+  
+   allResup2$LogEnrich <-  log2(allResup2$Significant/allResup2$Expected)
+  
   #tt$table$logFC <- -log2(tt$table$logFC)
-  colnames(allResup2) <- c("GO.ID","Term","Annotated","Significant","Expected","pvalCutOff","LogEnrich")
+  
+   colnames(allResup2) <- c("GO.ID","Term","Annotated","Significant","Expected","pvalCutOff","LogEnrich")
   
   
   
   
   
-  ga <- genesInTerm(go_up_bp) # GenesAnnotated | list of genes per go-terms
-  ga <- ga[allResup2$GO.ID] # eliminate missing terms
-  ga2 <- ga
-  names(ga) <- NULL
-  allResup2$GenesAnnotated <- ga
+   ga <- genesInTerm(go_up_bp) # GenesAnnotated | list of genes per go-terms
+   ga <- ga[allResup2$GO.ID] # eliminate missing terms
+   ga2 <- ga
+   names(ga) <- NULL
+   allResup2$GenesAnnotated <- ga
 
   
     
-  gadf <- ldply (ga2, data.frame)
-  colnames(gadf) <- c("GO.ID","GeneID")
-  ttdf <- as.data.frame(tt)
-  TTgoid <- inner_join(gadf,ttdf,c("GeneID" = "GeneID" ))
-  xs <- TTgoid [,c("FDR", "logFC")] # significant stats subset
-  xs <- subset(xs, FDR < 0.05)
-  allResup2$GenesSignificant <- lapply(allResup2$GenesAnnotated, intersect, rownames(xs)) # extract genes
+   gadf <- ldply (ga2, data.frame)
+   colnames(gadf) <- c("GO.ID","GeneID")
+   ttdf <- as.data.frame(tt)
+   TTgoid <- inner_join(gadf,ttdf,c("GeneID" = "GeneID" ))
+   xs <- TTgoid [,c("GeneID","FDR", "logFC")] # significant stats subset
+   xs <- subset(xs, FDR < 0.05)
+   #allResup2$GenesSignificant <- lapply(allResup2$GenesAnnotated, intersect, rownames(xs)) # extract genes
+   allResup2$GenesSignificant <- lapply(allResup2$GenesAnnotated, intersect, xs$GeneID) # extract genes
   
   
   
-  
-  goidlist <- allResup2$GO.ID
+   goidlist <- allResup2$GO.ID
   
   # TTgoidnd <- TTgoid[!d,]
   
   
-  lsfc <- list()
-  lspadj <- list()
-  i <- 1
-  for(Gid in goidlist) {
+   lsfc <- list()
+   lspadj <- list()
+   i <- 1
+   for(Gid in goidlist) {
     
   #  print(Gid)
-    Fgoid <-  filter(TTgoid,TTgoid$GO.ID == Gid)
+     Fgoid <-  filter(TTgoid,TTgoid$GO.ID == Gid)
     #print(Fgoid)
-    lsfc[i] <- list(Fgoid$logFC)
-    lspadj[i] <- list(Fgoid$FDR)
-    i <- i +1
-    }
+     lsfc[i] <- list(Fgoid$logFC)
+     lspadj[i] <- list(Fgoid$FDR)
+     i <- i +1
+     }
   #print(i)
   
-  allResup2$log2Foldchange <- lsfc
-  allResup2$padj <- lspadj
+   allResup2$log2Foldchange <- lsfc
+   allResup2$padj <- lspadj
   # 
   
 
   
   
-  x <- subset(allResup2,pvalCutOff <= 0.05 & Significant > 20)
-  x <- x[order(-x$LogEnrich),]
+   x <-  allResup2  #subset(allResup2,pvalCutOff <= 0.05 & Significant > 20)
+   x <- x[order(-x$LogEnrich),]
+   
+   #basic_plot2 <- ggplot(x, aes(x = reorder(Term,LogEnrich),y= LogEnrich)) +
+  #   geom_bar(stat = "identity", fill = graphfill) + coord_flip() +
+   #  labs(title = paste0("            SIC vs SIC_TM \n",ont," ",updown," up and down regulated genes"),
+  #        x = "Term",
+  #        y = "LogEnrich")
+   
+   
+   #print(basic_plot2)
+   
+   
   
-   cell.plot(x = setNames(x$LogEnrich,x$Term),
-            cells = x$log2Foldchange ,
-            main = paste0(" GO enrichment (SiC vs SiC_TM) ",onto),
-            x.mar = c(0.4, 0), 
-            key.n = 7, 
-            y.mar = c(0.1, 0), 
-            cex = 1.6, 
-            cell.outer = 3, 
-            bar.scale = 0.7, 
-            space = 0.09)
+    cell.plot(x = setNames(x$LogEnrich,x$Term),
+             cells = x$log2Foldchange ,
+             main = paste0(" GO enrichment (SiC vs SiC_TM) ",onto),
+             x.mar = c(0.4, 0), 
+             key.n = 7, 
+             y.mar = c(0.1, 0), 
+             cex = 1.6, 
+             cell.outer = 3, 
+             bar.scale = 0.7, 
+             space = 0.09)
   
   
  # dev.off()
 
   
 #  pdf(file = "/home/bjamshidikia/Desktop/BIOINFORMATICS_jacobi/graphs/cellplot/S3.pdf")
-   sym.plot( x= setNames(x$LogEnrich,x$Term),
-           cells = x$log2Foldchange,
-            x.annotated = x$Annotated,
-            main = paste0(" GO enrichment (SiC vs SiC_TM) ",onto),
-            x.mar = c(0.47,0),
-            key.n = 7,
-            cex = 1.6,
-            axis.cex = 0.8,
-            group.cex = 0.7,
-           y.mar = c(0.3, 0) 
-#           bar.scale = 0.2,
-#           space = 0.02
-  )
+    sym.plot( x= setNames(x$LogEnrich,x$Term),
+            cells = x$log2Foldchange,
+             x.annotated = x$Annotated,
+             main = paste0(" GO enrichment (SiC vs SiC_TM) ",onto),
+             x.mar = c(0.47,0),
+             key.n = 7,
+             cex = 1.6,
+             axis.cex = 0.8,
+             group.cex = 0.7,
+            y.mar = c(0.1, 0), 
+           bar.scale = 0.7
+          #space = 1
+   )
   
 }
 dev.off()  
